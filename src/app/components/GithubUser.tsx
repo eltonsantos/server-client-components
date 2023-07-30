d21512e3e26f3672f98475f0cbc51828a04470d1
Elton Santos <eltonaxl@hotmail.com>
2023-07-29 23:35:36 -0300

    Add Suspense and component GithubUser

---
+++ src/app/components/GithubUser.tsx
@@ -0,0 +1,14 @@
+export interface GithubUserProps {
+  username: string
+} 
+
+export async function GithubUser({ username }: GithubUserProps) {
+
+  const response = await fetch(`https://api.github.com/users/${username}`)
+  const user = await response.json()
+
+  await new Promise(resolve => setTimeout(resolve, 3000))
+  return (
+    <p>{JSON.stringify(user)}</p>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,16 @@
-export default function Home() {
+import { Suspense } from "react";
+import { GithubUser } from "./components/GithubUser";
+
+export default async function Home() {
+
   return (
-    <h1>Hello World</h1>
+    <div>
+      <Suspense fallback={<p>Carregando...</p>}>
+        <GithubUser username="eltonsantos" />
+      </Suspense>
+      <Suspense fallback={<p>Carregando...</p>}>
+        <GithubUser username="maykbrito" />
+      </Suspense>
+    </div>
   )
 }
